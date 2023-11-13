4f2b6a43f122143071b5e42c7ae03cf8d54f3918
teerapong155 <teerapong.pu@rmuti.ac.th>
2023-11-13 10:51:45 +0700

    changes

--- Home_Page/Table_Contestant.php
+++ Home_Page/Table_Contestant.php
@@ -5,49 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/Project_E-SPORT_THE_END/HOME_Page/CSS_Home_Page/CSS_Table_Contestant.css">
     <title>Document</title>
 </head>
 <body>
     <div class="title_Table_Contestant">
         ตารางสมาชิกของทีมเข้าแข่งขัน
     </div>
-    <div class="card bg-secondary text-white" style="width:400px">
-        <div class="Logo_Team">
-            <img src="../images/news2.png" width="100" height="100">
-        </div>
-        <div class="Detail_Team">
-            <p>
-                <label>เข้าร่วม:</label>
-            </p>
-            <p>
-                <label>ชื่อทีม:</label>
-            </p>
-            <p>
-                <label>ข้อมูลทีม:</label>
-            </p>
-            
+    <div class="detail">
+        <div class="card bg-secondary text-white" style="width:400px">
+            <div class="Logo_Team">
+                <img src="../images/news2.png" width="100px" height="100px">
+            </div>
+            <div class="Detail_Team">
+                <p>
+                    <label>เข้าร่วม:</label>
+                </p>
+                <p>
+                    <label>ชื่อทีม:</label>
+                </p>
+                <p>
+                    <label>ข้อมูลทีม:</label>
+                </p>
+                
+            </div>
         </div>
     </div>
     <div class="container">
         <div class="Contestant">
             <label>สมาชิกทีม</label>
             <div class="row">
-                <div class="col-md-4">
-                    <div class="card bg-secondary" style="height:200px; width:200px">
-                        <img src="../images/news2.png" width="100" height="100">
-                    </div>
+
+            <div class="card bg-secondary">
+                <img src="../images/news2.png" alt="Avatar" style="width:165px; height:200px;" class="logo">
+                <div class="member">
+                    <h4><b>John Doe</b></h4> 
+                    <p></p> 
                 </div>
-                <div class="col-md-4">
-                    <div class="card bg-secondary" style="height:200px; width:200px">
-                        <img src="../images/news2.png" width="100" height="100">
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card bg-secondary" style="height:200px; width:200px">
-                        <img src="../images/news2.png" width="100" height="100">
-                    </div>
                 </div>
             </div>
+
         </div>
     </div>
 </body>
